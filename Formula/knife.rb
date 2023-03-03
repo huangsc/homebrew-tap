dcc04515f3ac085663f3cc7db4046eef95d6e140
shicheng huang <huangshicheng@bituniverse.org>
2023-03-03 15:53:41 +0800

    update SHA256

--- Formula/knife.rb
+++ Formula/knife.rb
@@ -2,7 +2,7 @@ class Knife < Formula
   desc "提升软件开发效率的小工具"
   homepage "https://github.com/huangsc/knife"
   url "https://github.com/huangsc/knife/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "601632bb047ac2d76a31410d4be9fef8cb548accd20da697c37dffd510b4ea9c"
+  sha256 "e76456c7216129eef2167d8863c06225c4de03434072d3c76687583192c44334"
   version "0.0.1"
   def install
     bin.install "knife"
